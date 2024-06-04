191d843794e299730268af883641dad2907c7a85
Huntercity63 <anhnphe171575@fpt.edu.vn>
2024-06-04 22:59:55 +0700

    ajaxcartdetails

--- src/java/DAL/DAOUser.java
+++ src/java/DAL/DAOUser.java
@@ -85,7 +85,7 @@ public class DAOUser extends DBContext {
         public User getUserByLogin(String username) {
         User u = null;
         String sql = "select u.UserID,u.first_name,u.last_name,u.phone,u.email,u.address,u.username,u.password,\n"
-                + "u.dob,u.gender,u.status, u.role,u.securityID,u.securityAnswer,s.security_question from [User] u\n"
+                + "u.dob,u.gender,u.status, u.RoleID,u.securityID,u.securityAnswer,s.security_question from [User] u\n"
                 + "inner join SecurityQuestion s on u.securityID=s.securityID"
                 + "inner join [Role] r on r.RoleID=u.RoleID"
                 + " where username =?";
